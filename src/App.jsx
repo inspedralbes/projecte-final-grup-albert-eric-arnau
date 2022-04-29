3f45f1a514a67d2fdb4ff0c070bcb86e29ec7f76
Eric <a19ericlecas@inspedralbes.cat>
2022-04-29 10:09:00 +0200

    Renamed extension App from js to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Provider } from "react-redux";
 import { ChatView } from "./components/chat";
 import { store } from "./store/store";
+import AppRouter from "./routers/AppRouter";
 
 function App() {
   return (
     <Provider store={store}>
-      <ChatView />
+      <AppRouter />
     </Provider>
   );
 }
