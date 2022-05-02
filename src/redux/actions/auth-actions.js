5d043b4fc7fcfdb2ab8848132c3191dc479d63e5
Albert <a19albchavas@inspedralbes.cat>
2022-05-02 09:54:53 +0200

    Cambio de nombres de los types

--- src/redux/actions/auth-actions.js
+++ src/redux/actions/auth-actions.js
@@ -2,7 +2,7 @@ import { authTypes } from "../action-types";
 //esto esta hecho con el copilot, para recordarte lo de los actions xdd, en la carpeta thunk va lo mismo pero todo lo que sea async
 export const login = (email, password) => {
   return {
-    type: authTypes.login,
+    type: authTypes.LOGIN,
     payload: {
       email,
       password,
@@ -12,6 +12,6 @@ export const login = (email, password) => {
 
 export const logout = () => {
   return {
-    type: authTypes.logout,
+    type: authTypes.LOGOUT,
   };
 };
