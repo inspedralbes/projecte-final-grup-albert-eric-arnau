cf7269457eca87ced1cfc86cb3b09fe8579714e6
Eric <60893006+a19ericlecas@users.noreply.github.com>
2022-05-02 10:15:45 +0200

    styles chat message creado

--- src/components/chat/message/chat-message.component.jsx
+++ src/components/chat/message/chat-message.component.jsx
@@ -1,30 +1,7 @@
 import React from "react";
-import { createStyles, Text, Avatar, Group, Paper } from "@mantine/core";
+import { Text, Avatar, Group, Paper } from "@mantine/core";
 
-const useStyles = createStyles((theme) => ({
-  comment: {
-    padding: `${theme.spacing.lg}px ${theme.spacing.xl}px`,
-  },
-
-  body: {
-    paddingLeft: 54,
-    paddingTop: theme.spacing.sm,
-    fontSize: theme.fontSizes.sm,
-  },
-
-  content: {
-    "& > p:last-child": {
-      marginBottom: 0,
-    },
-  },
-
-  message: {
-    width: "fit-content",
-    maxWidth: "50%",
-    margin: "5px",
-    padding: "10px 20px 10px 5px",
-  },
-}));
+import { useStyles } from "./chat-message.styles";
 
 function ChatMessage({ messageData = null }) {
   const { classes } = useStyles();
---
+++ src/components/chat/message/chat-message.styles.js
@@ -0,0 +1,10 @@
+import {createStyles} from "@mantine/core";
+
+export const useStyles = createStyles({
+  message: {
+    width: "fit-content",
+    maxWidth: "50%",
+    margin: "5px",
+    padding: "10px 20px 10px 5px",
+  },
+});
