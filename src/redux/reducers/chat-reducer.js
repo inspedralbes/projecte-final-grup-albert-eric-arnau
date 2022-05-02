bf743a3436c82e49fe1c2a433bb84319a9cb5afb
Albert <a19albchavas@inspedralbes.cat>
2022-05-02 10:17:43 +0200

    Añadido chat reducer

---
+++ src/redux/reducers/chat-reducer.js
@@ -0,0 +1,27 @@
+import { ChartDonut } from "tabler-icons-react";
+import { chatTypes } from "../action-types";
+
+const initialState = [];
+
+const chatReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case chatTypes.MESSAGE_SEND:
+      return {
+        payload: action.payload,
+      };
+    case chatTypes.MESSAGE_RECEIVE:
+      return {
+        user: state.user,
+        message: state.message,
+        time: state.time,
+        imgLink: state.imgLink,
+      };
+    case chatTypes.RECEIVE_MESSAGES:
+      return {
+        ...state,
+      };
+    case chatTypes.default:
+      return state;
+  }
+};
+export default chatReducer;
