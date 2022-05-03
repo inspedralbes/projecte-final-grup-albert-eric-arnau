bc70c3f429b3220c2abd0eea5c444b976ffbb179
Arnau <a19arnortbri@inspedralbes.cat>
2022-05-03 12:51:59 +0200

    UserButton creado

---
+++ src/components/user-button/user-button.component.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  UnstyledButton,
+  UnstyledButtonProps,
+  Group,
+  Avatar,
+  Text,
+} from '@mantine/core';
+import { ChevronRight } from 'tabler-icons-react';
+import { useStyles } from './user-button.styles';
+
+
+function UserButton({ image, name, email, icon, ...others }) {
+  const { classes } = useStyles();
+
+  return (
+    <UnstyledButton className={classes.user} {...others}>
+      <Group>
+        <Avatar src={image} radius="xl" />
+
+        <div style={{ flex: 1 }}>
+          <Text size="sm" weight={500}>
+            {name}
+          </Text>
+
+          <Text color="dimmed" size="xs">
+            {email}
+          </Text>
+        </div>
+
+        {icon || <ChevronRight size={14} />}
+      </Group>
+    </UnstyledButton>
+  );
+}
+export default UserButton;
