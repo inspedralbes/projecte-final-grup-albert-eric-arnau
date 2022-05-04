ccd1513995d13533e9043b2601d80910f2b5be82
Albert <a19albchavas@inspedralbes.cat>
2022-05-04 13:01:17 +0200

    Cambios en el sistema de rutas

---
+++ src/routers/app/public-routes.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Navigate } from "react-router-dom";
+
+function PublicRoutes({ children, isLogged }) {
+  return isLogged ? <Navigate to="/chat" /> : children;
+}
+
+export default PublicRoutes;
