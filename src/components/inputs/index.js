ce451ea81ca65da26745c98cbc848211fbbc0025
Eric <60893006+a19ericlecas@users.noreply.github.com>
2022-05-16 13:02:58 +0200

    deleted components of inputs

--- src/components/inputs/index.js
+++
@@ -1,3 +0,0 @@
-export { default as FloatingLabelInput } from "./floating-label-input/floating-label-input.component";
-export { default as ForgotPasswordInput } from "./forgot-password-input";
-export { default as PasswordValidatorInput } from "./password-validator-input/password-validator-input.component";
