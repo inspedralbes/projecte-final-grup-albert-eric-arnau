3aa878f918f6ccaaf78e67a8bff80018d95fcf19
Eric <60893006+a19ericlecas@users.noreply.github.com>
2022-05-16 09:41:03 +0200

    registration and login thunk

--- src/redux/thunk/auth-thunk.js
+++ src/redux/thunk/auth-thunk.js
@@ -7,7 +7,46 @@ import { auth } from "../../firebase/firebaseConfig";
 export const loginThunk = (email, password) => {
   return async (dispatch) => {
     try {
-      const user = await auth.signInWithEmailAndPassword(email, password);
+      const { uid } = await auth.signInWithEmailAndPassword(email, password);
+      const response = await fetch(`${process.env.API_URL}/user/${uid}`);
+      const user = await response.json();
+      dispatch(loginAction(user));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+export const autoLoginThunk = ({ uid }) => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch(`${process.env.API_URL}/user/${uid}`);
+      const user = await response.json();
+      dispatch(loginAction(user));
+    } catch (err) {
+      console.log(err);
+    }
+  };
+};
+
+export const registerThunk = (username, email, displayName, password) => {
+  return async (dispatch) => {
+    try {
+      const { uid } = await auth.createUserWithEmailAndPassword(
+        email,
+        password
+      );
+      const newUser = {
+        name: displayName,
+        userID: uid,
+        username,
+        email,
+      };
+      const response = await fetch(`${process.env.API_URL}/user/register`, {
+        method: "POST",
+        body: JSON.stringify(newUser),
+      });
+      const user = await response.json();
       dispatch(loginAction(user));
     } catch (err) {
       console.log(err);
