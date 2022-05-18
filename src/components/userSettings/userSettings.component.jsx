e11937eeb63e15e3166c257ea2c05646828edc1d
Arnau <a19arnortbri@inspedralbes.cat>
2022-05-18 12:55:14 +0200

    User setings (Casi acabado falta color picker)

--- src/components/userSettings/userSettings.component.jsx
+++ src/components/userSettings/userSettings.component.jsx
@@ -1,9 +1,61 @@
-import { TextInput, PasswordInput, Tooltip, Center, Text } from "@mantine/core";
-import { InfoCircle } from "tabler-icons-react";
+import {
+  TextInput,
+  Tooltip,
+  Center,
+  Image,
+  Text,
+  Grid,
+  Button,
+  Paper,
+  Textarea,
+  Indicator,
+} from "@mantine/core";
+import { InfoCircle, Edit } from "tabler-icons-react";
+import { useStyles } from "./userSettings.styles";
 function UserSettings() {
-  const rightSection = (
+  const { classes } = useStyles();
+  const username = (
     <Tooltip
-      label="We store your data securely"
+      label="Your unique username"
+      placement="end"
+      withArrow
+      transition="pop-bottom-right">
+      <Text color="dimmed" sx={{ cursor: "help" }}>
+        <Center>
+          <InfoCircle size={18} />
+        </Center>
+      </Text>
+    </Tooltip>
+  );
+  const displayName = (
+    <Tooltip
+      label="The name other useres will see"
+      placement="end"
+      withArrow
+      transition="pop-bottom-right">
+      <Text color="dimmed" sx={{ cursor: "help" }}>
+        <Center>
+          <InfoCircle size={18} />
+        </Center>
+      </Text>
+    </Tooltip>
+  );
+  const email = (
+    <Tooltip
+      label="Email address"
+      placement="end"
+      withArrow
+      transition="pop-bottom-right">
+      <Text color="dimmed" sx={{ cursor: "help" }}>
+        <Center>
+          <InfoCircle size={18} />
+        </Center>
+      </Text>
+    </Tooltip>
+  );
+  const password = (
+    <Tooltip
+      label="Your password"
       placement="end"
       withArrow
       transition="pop-bottom-right">
@@ -16,11 +68,71 @@ function UserSettings() {
   );
 
   return (
-    <TextInput
-      rightSection={rightSection}
-      label="Tooltip shown on icon hover"
-      placeholder="Your email"
-    />
+    <Paper shadow="xl" radius="md" className={classes.paper}>
+      <Grid gutter={40} className={classes.wrapper}>
+        <Grid.Col span={6}>
+          <div className={classes.container}>
+            <center>
+              <Indicator
+                position="bottom-end"
+                color="orange"
+                inline
+                label={<Edit className={classes.editButton} />}
+                size={30}>
+                <Image
+                  src="https://www.disponalencasa.com/pub/media/catalog/product/cache/4025f56c98cb88143bb53de4d18da868/m/o/monster-juice-mango-loco.jpg"
+                  className={classes.image}
+                  radius="xl"
+                />
+              </Indicator>
+            </center>
+          </div>
+        </Grid.Col>
+        <Grid.Col span={6}>
+          <div className={classes.container}>
+            <Textarea
+              placeholder="Your description"
+              label="Short description"
+            />
+          </div>
+        </Grid.Col>
+        <Grid.Col span={6}>
+          <div className={classes.container}>
+            <TextInput
+              rightSection={username}
+              label="@username"
+              placeholder="@example"
+            />
+            <TextInput
+              rightSection={displayName}
+              label="Display name"
+              placeholder="example_123"
+            />
+          </div>
+        </Grid.Col>
+        <Grid.Col span={6}>
+          <div className={classes.container}>
+            <TextInput
+              rightSection={email}
+              label="Email"
+              placeholder="123@example.com"
+            />
+            <TextInput
+              rightSection={password}
+              label="Password"
+              placeholder="******"
+            />
+          </div>
+        </Grid.Col>
+        <Grid.Col span={12}>
+          <center>
+            <Button variant="gradient" gradient={{ from: "orange", to: "red" }}>
+              Submit
+            </Button>
+          </center>
+        </Grid.Col>
+      </Grid>
+    </Paper>
   );
 }
 
