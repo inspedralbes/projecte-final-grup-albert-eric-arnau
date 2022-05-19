bf3942abd9da5225fc7d539fa9c4af65daaca4db
Albert <a19albchavas@inspedralbes.cat>
2022-05-19 08:49:06 +0200

    Cambios en la estructura

--- src/views/group-finder/components/group-item/group-item.component.jsx
+++ src/views/group-finder/components/group-item/group-item.component.jsx
@@ -1,4 +1,4 @@
-import { Avatar, Group, Text, ActionIcon, Menu } from "@mantine/core";
+import { Avatar, Group, Text, ActionIcon, Button } from "@mantine/core";
 import {
   Pencil,
   Messages,
@@ -17,9 +17,6 @@ function GroupItem({ item }) {
             <Text size="sm" weight={500}>
               {item.name}
             </Text>
-            <Text color="dimmed" size="xs">
-              {item.job}
-            </Text>
           </div>
         </Group>
       </td>
@@ -30,26 +27,10 @@ function GroupItem({ item }) {
         </Text>
       </td>
       <td>
-        <Text size="sm">${item.rate.toFixed(1)} / hr</Text>
-        <Text size="xs" color="dimmed">
-          Rate
-        </Text>
-      </td>
-      <td>
         <Group spacing={0} position="right">
-          <ActionIcon>
-            <Pencil size={16} />
-          </ActionIcon>
-          <Menu transition="pop" withArrow placement="end">
-            <Menu.Item icon={<Messages size={16} />}>Send message</Menu.Item>
-            <Menu.Item icon={<Note size={16} />}>Add note</Menu.Item>
-            <Menu.Item icon={<ReportAnalytics size={16} />}>
-              Analytics
-            </Menu.Item>
-            <Menu.Item icon={<Trash size={16} />} color="red">
-              Terminate contract
-            </Menu.Item>
-          </Menu>
+          <Button>
+            Group info
+          </Button>
         </Group>
       </td>
     </tr>
