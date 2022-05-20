6854d7a5fa35e792d87275ac90f1cb8c0879991d
Eric <60893006+a19ericlecas@users.noreply.github.com>
2022-05-20 10:28:38 +0200

    - Bugfix on margin in body

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
+import { MantineProvider } from "@mantine/core";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import { AppRouter } from "./routers";
 
 function App() {
   return (
-    <Provider store={store}>
-      <AppRouter />
-    </Provider>
+    <MantineProvider withNormalizeCSS>
+      <Provider store={store}>
+        <AppRouter />
+      </Provider>
+    </MantineProvider>
   );
 }
 
